3e2e4be6294412dc573521cc3f2bf8cab10fe45a
Belhachemi Redha <redha.b@codeur.online>
2018-12-20 16:52:49 +0100

    changement js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
    }
 }
 
-function verifMail(email_destinataire)
+function verifMail(mail_destinataire)
 {
    var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
    if(!regex.test(champ.value))
@@ -26,13 +26,13 @@ function verifMail(email_destinataire)
    {
       surligne(champ, false);
       return true;
-   }
+   }                                                                                                                                    
 }
 function verifForm(f)
 {
    
    var mailOk = verifMail(votre_email);
-   var mailOk = verifMail(email_destinataire);
+   var mailOk = verifMail(mail_destinataire);
    
    
    if(mailOk)
@@ -42,7 +42,7 @@ function verifForm(f)
       alert("Veuillez remplir correctement tous les champs");
       return false;
    }
-   {
+   { 
     if(erreur)
        champ.style.backgroundColor = "#FF0000";
     else
