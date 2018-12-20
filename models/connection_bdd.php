c9cd6b37233b35c225e3c4cf9bc269d03ac28fb5
Keolite <contact@keolite.fr>
2018-12-20 09:03:54 +0000

    [bdd] Correction du nom de alm bas ed eonnées

--- models/connection_bdd.php
+++ models/connection_bdd.php
@@ -1,4 +1,4 @@
  <?php 
-$basedonne = new PDO("mysql:host=localhost;dbname=transfert; charset=utf8", "root", "online@2017");
+$basedonne = new PDO("mysql:host=localhost;dbname=transfer; charset=utf8", "root", "online@2017");
 $baseurl = ( strpos($_SERVER["HTTP_HOST"], ":8080") === false ) ? "http://localhost/transfer/" : "http://localhost:8080/transfer/";
  
